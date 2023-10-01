8847072f662c70cbf5121c898f71cd737041b717
Dunithuyduong <144688417+Dunithuyduong@users.noreply.github.com>
2023-10-01 14:45:23 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+var typed = new Typed('.element',{
+strings: ["Designer.", "Web Designer.", "Code Developer.","Apps Designer.","Dancer."],
+typeSpeed:20,
+backSpeed:20,
+loop:true,
+});
